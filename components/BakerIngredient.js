ed66948a8e015db1c3ad709b72be760fe31401c5
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-01-23 02:24:25 +0100

    Update style

--- components/BakerIngredient.js
+++ components/BakerIngredient.js
@@ -4,6 +4,9 @@ import Poster from './Poster'
 
 export default function BakerIngredient(props) {
   const { name, image, models, isHidden } = props
+
+  const hasManyModels = models.length > 1
+
   return (
     <div className={styles.BakerIngredient}>
       <Poster className={styles.poster} imageUrl={image} />
@@ -17,9 +20,23 @@ export default function BakerIngredient(props) {
               <b>{model.name}</b>
               <label>Amount: {model.amount}</label>
               <label>Price: {model.price}</label>
+              <br />
+              {hasManyModels && (
+                <div className={styles.buttons}>
+                  <button>Restock</button>
+                  <button>Remove</button>
+                </div>
+              )}
             </div>
           ))}
         </div>
+
+        {!hasManyModels && (
+          <div className={styles.buttons}>
+            <button>Restock</button>
+            <button>Remove</button>
+          </div>
+        )}
       </div>
     </div>
   )
