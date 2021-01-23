19637146cb734b14d9430c5a4c6e1f7a0924e81b
Raufoon <minhaz.raufoon.1567@gmail.com>
2021-01-23 03:07:19 +0100

    Update style

--- components/BakerIngredient.js
+++ components/BakerIngredient.js
@@ -1,11 +1,13 @@
-import { Fragment } from 'react'
 import styles from '../styles/BakerIngredient.module.css'
+import BuyButton from './BuyButton'
+import CrossButton from './CrossButton'
+import EditButton from './EditButton'
+import HideButton from './HideButton'
+import ShowButton from './ShowButton'
 import Poster from './Poster'
 
 export default function BakerIngredient(props) {
   const { name, image, models, isHidden } = props
-
-  const hasManyModels = models.length > 1
 
   return (
     <div className={styles.BakerIngredient}>
@@ -17,26 +19,19 @@ export default function BakerIngredient(props) {
         <div className={styles.model}>
           {models.map((model, index) => (
             <div key={model.name || index} className={styles.modelInfo}>
-              <b>{model.name}</b>
+              <b>{model.name}&nbsp;</b>
               <label>Amount: {model.amount}</label>
               <label>Price: {model.price}</label>
               <br />
-              {hasManyModels && (
-                <div className={styles.buttons}>
-                  <button>Restock</button>
-                  <button>Remove</button>
-                </div>
-              )}
+              <div className={styles.buttons}>
+                <BuyButton />
+                {isHidden ? <ShowButton /> : <HideButton />}
+                <EditButton />
+                <CrossButton />
+              </div>
             </div>
           ))}
         </div>
-
-        {!hasManyModels && (
-          <div className={styles.buttons}>
-            <button>Restock</button>
-            <button>Remove</button>
-          </div>
-        )}
       </div>
     </div>
   )
